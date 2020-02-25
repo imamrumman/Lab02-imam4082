f8774b0600a1c36883372992a9ec8236c6078fbf
Rumman Imam <imamrumman@gmail.com>
2020-02-25 13:52:13 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,13 @@
-
 provider "google" {
   credentials ="${file("service-account.json")}"
   project = "{{red-reference-267601}}"
   region  = "us-central1"
   zone    = "us-central1-c"
 }
-
-
+0 references
 Source "google_compute_instance" "svc-1" {
   name         = "imam4082-testvm1"
   machine_type = "f1-micro"
-  zone         = "us-central1-c"
   
   boot_disk {
     initialize_params {
