7f3bf29b6cd175aee40e8764b50f472808e2959d
Rumman Imam <imamrumman@gmail.com>
2020-02-24 18:58:21 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,3 +4,9 @@ provider "google" {
   region  = "us-central1"
   zone    = "us-central1-c"
 }
+
+
+Source "google_compute_instance" "svc-1" {
+  name         = "imam4082-testvm1"
+  machine_type = "f1-micro"
+  zone         = "us-central1-c"
