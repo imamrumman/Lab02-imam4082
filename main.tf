24f6a561109728c5e701b147d97807038a003033
Rumman Imam <imamrumman@gmail.com>
2020-02-25 12:49:38 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,3 +11,17 @@ Source "google_compute_instance" "svc-1" {
   name         = "imam4082-testvm1"
   machine_type = "f1-micro"
   zone         = "us-central1-c"
+  
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral IP
+    }
+  }
